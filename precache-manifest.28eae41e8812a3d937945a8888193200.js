5bddca0de6d776af1b836aebeb9a3ae1a7b06adb
Patrick Brown <pjbrown05@gmail.com>
2020-09-24 14:58:46 -0500

    Updates

---
+++ precache-manifest.28eae41e8812a3d937945a8888193200.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148f94a09377fd48148e07a11bae1810",
+    "url": "/PatrickBrown/index.html"
+  },
+  {
+    "revision": "42b554dea39726ddf2de",
+    "url": "/PatrickBrown/static/css/main.ec85c68e.chunk.css"
+  },
+  {
+    "revision": "c0796303b5890f0e69b3",
+    "url": "/PatrickBrown/static/js/2.ef1946f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PatrickBrown/static/js/2.ef1946f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42b554dea39726ddf2de",
+    "url": "/PatrickBrown/static/js/main.b842c078.chunk.js"
+  },
+  {
+    "revision": "284f97ca299225fc8b8a",
+    "url": "/PatrickBrown/static/js/runtime-main.1ff8ae22.js"
+  },
+  {
+    "revision": "278662705f90d92e5d605a7e149b2995",
+    "url": "/PatrickBrown/static/media/background.27866270.jpg"
+  }
+]);
