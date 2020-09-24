0f21d77187a9516d87b355eb3f3e5c093e78cf56
Patrick Brown <pjbrown05@gmail.com>
2020-09-24 15:05:48 -0500

    router fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { HashRouter, HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Portfolio from './pages/Portfolio';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -8,15 +8,15 @@ import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <Router basename="/">
       <div className="container">
         <Navbar />
         <Route exact path="/" component={About} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
+        <Route path="/about" component={About} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
       </div>
-    </HashRouter>
+    </Router>
   );
 }
 
