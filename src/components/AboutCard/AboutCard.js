4ee8b24b4ffaa820e647a5095918c0d6287f5aa3
Patrick Brown <pjbrown05@gmail.com>
2020-09-28 01:01:08 -0500

    about styling, contact responsive

--- src/components/AboutCard/AboutCard.js
+++ src/components/AboutCard/AboutCard.js
@@ -7,16 +7,23 @@ const AboutCard = () => {
     <div className="about-card-container">
       <div className="about-card-box">
         <div className="about-card">
-          <img src={profilepic} className="profile-img" alt="Patrick Brown" />
           <div className="about-card-body">
+            <img src={profilepic} className="profile-img" alt="Patrick Brown" />
             <h1 className="about-card-title">About</h1>
             <hr className="about-break"></hr>
             <h3 className="about-card-text">
-              <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Full stack developer (MERN) with a background in fire sprinkler designer.</p>
-              <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I have an Associates of Applied Science in Engineering CAD Technology from Hennepin Tech and recently completed the MERN Full Stack boot camp from the University of Minnesota.</p>
-              <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;During the course I worked as a team of two to develop an app, Farmanger, designed to track plants and the space where they're being grown. Technologies include: JavaScript, HTML/CSS, Bootstrap, API routing, and MySQL.</p>
-              <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;As a fire sprinkler designer, I have managed and designed large projects (e.g. Life Time Fitness, Edina, MN) and worked collaboratively all my career with a high level of communication and organization.</p>
-              <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I am eager to learn and excited to create. I love to problem solve and have a great appreciation when design meets functionality.</p>
+              <p>Full stack developer (MERN) with a background in fire sprinkler designer.</p>
+              <br></br>
+
+              <p>I have an Associates of Applied Science in Engineering CAD Technology from Hennepin Tech and recently completed the MERN Full Stack boot camp from the University of Minnesota.</p>
+              <br></br>
+
+              <p>During the course I worked as a team of two to develop an app, Farmanger, designed to track plants and the space where they're being grown. Technologies include: JavaScript, HTML/CSS, Bootstrap, API routing, and MySQL.</p>
+              <br></br>
+
+              <p>As a fire sprinkler designer I have managed and designed large projects (e.g. Life Time Fitness, Edina, MN) and worked collaboratively all my career with a high level of communication and organization.</p>
+              <br></br>
+              <p>I am eager to learn and excited to create. I love to problem solve and have a great appreciation when design meets functionality.</p>
             </h3>
           </div>
 
@@ -27,3 +34,5 @@ const AboutCard = () => {
 }
 
 export default AboutCard
+
+// &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
