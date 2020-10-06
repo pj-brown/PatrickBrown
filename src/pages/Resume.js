43e54aa7e4024fe6c29c7835bd4ca4898963019d
Patrick Brown <pjbrown05@gmail.com>
2020-10-06 11:39:38 -0500

    updated resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -6,7 +6,7 @@ const Resume = () => {
   return (
     <div className="resume"><h1></h1>
       <Iframe />
-      <a className="googl-link" target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/1YMm-RnOHfD_APtPvEILVfJb9TWw__dPa7Iz-xY9mAio/edit?usp=sharing"><h5>Google Docs Version</h5></a>
+      <a className="google-link" target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/1YMm-RnOHfD_APtPvEILVfJb9TWw__dPa7Iz-xY9mAio/edit?usp=sharing"><h5>Google Docs Version</h5></a>
 
     </div>
   )
